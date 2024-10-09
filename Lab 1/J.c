216d7de14c76a80e025ef0a6493904c6b72f363f
AJ Tech City <arunjai12456@icloud.com>
2024-10-09 12:46:32 +0100

    Task J Started

---
+++ Lab 1/J.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+
+    int x, nat_x;
+
+    printf("Enter X: ");
+    scanf("%d", &x);
+
+    nat_x = log(x);
+    exp(nat_x);
+
+    return 0;
+}
