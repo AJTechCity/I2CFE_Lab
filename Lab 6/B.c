8cc554c540781a736f5dc2d9b419979182628fa0
AJ Tech City <arunjai12456@icloud.com>
2024-11-14 18:23:14 +0000

    Task B Complete

---
+++ Lab 6/B.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    int i;
+    int x[10];
+
+    for(i=0;i<=9;i++){
+        x[i] = i*10;
+        printf("Setting x[%d] = %d\n", i, x[i]);
+    }
+
+    for(i=9;i>=0;i--){
+        printf("x[%d] = %d\n", i, x[i]);
+    }
+
+    return 0;
+}
